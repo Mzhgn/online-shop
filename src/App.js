38f8b80dd1694341f57f1700b25b0a9beb9c7391
Mozhgan <mojganshah@gmail.com>
2024-11-14 15:39:07 +0330

    product context

--- src/App.js
+++ src/App.js
@@ -5,23 +5,39 @@ import Toast from "./Components/Toast/Toast";
 import "./App.css";
 import products from "./data/Products";
 import Cart from "./Components/Cart/Cart";
+import productsContext from "./Contexts/ProductsContext";
 
 export default function App() {
   const [allProducts, setAllProducts] = useState(products);
+  const [userCat, setUserCart] = useState([]);
+  const [isShowToast, setIsShowToast] = useState(false);
+  const [isShowCart, setIsShowCart] = useState(false);
   return (
     <div>
-      <Navbar />
+      <productsContext.Provider
+        value={{
+          allProducts,
+          userCat,
+          setUserCart,
+          isShowToast,
+          setIsShowToast,
+          isShowCart,
+          setIsShowCart,
+        }}
+      >
+        <Navbar />
 
-      <main className="pb-5">
-        <div className="container">
-          <h1 className="text-center main-title">All Products</h1>
-          {allProducts.map((product) => (
-            <ProductSection {...product} />
-          ))}
-        </div>
-      </main>
-      <Toast />
-      <Cart />
+        <main className="pb-5">
+          <div className="container">
+            <h1 className="text-center main-title">All Products</h1>
+            {allProducts.map(() => (
+              <ProductSection />
+            ))}
+          </div>
+        </main>
+        <Toast />
+        <Cart />
+      </productsContext.Provider>
     </div>
   );
 }
--- src/Components/productsSections/ProductSection.js
+++ src/Components/productsSections/ProductSection.js
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductSection.css";
+import productsContext from "../../Contexts/ProductsContext";
 
-function ProductSection({ title, info }) {
+function ProductSection() {
+  const contextData = useContext(productsContext);
   return (
-    <div className="row justify-content-center mt-5">
-      <h3 className="text-center">{title}</h3>
+    <>
+      {contextData.allProducts.map((productSection) => (
+        <div className="row justify-content-center mt-5">
+          <h3 className="text-center">{productSection.title}</h3>
 
-      {info.map((product) => (
-        <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
-          <div className="card py-3 px-3">
-            <div className="col-12 text-center">
-              <img
-                src="/images/1.jpg"
-                alt="Product Image"
-                className="card-img-top w-75"
-              />
-            </div>
-            <div className="card-body text-center">
-              <p className="card-text">{product.title}</p>
-              <p className="price">{product.price}$</p>
-              <br />
-              <a href="#" className="btn btn-danger">
-                Add to Cart
-              </a>
-              <a href="" className="btn btn-outline-dark mt-3 text-capitalize">
-                More Information
-              </a>
+          {productSection.info.map((product) => (
+            <div className="col-xl-3 col-lg-4 col-md-5 col-sm-10 mt-5">
+              <div className="card py-3 px-3">
+                <div className="col-12 text-center">
+                  <img
+                    src="/images/1.jpg"
+                    alt="Product Image"
+                    className="card-img-top w-75"
+                  />
+                </div>
+                <div className="card-body text-center">
+                  <p className="card-text">{product.title}</p>
+                  <p className="price">{product.price}$</p>
+                  <br />
+                  <a href="#" className="btn btn-danger">
+                    Add to Cart
+                  </a>
+                  <a
+                    href=""
+                    className="btn btn-outline-dark mt-3 text-capitalize"
+                  >
+                    More Information
+                  </a>
 
-              <p className="number">Number: {product.count}</p>
+                  <p className="number">Number: {product.count}</p>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       ))}
-    </div>
+    </>
   );
 }
 
