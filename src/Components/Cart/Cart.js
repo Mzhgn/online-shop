889a01eb91804e841931d96907843c8597421c66
Mozhgan <mojganshah@gmail.com>
2024-11-15 22:52:37 +0330

    adding context to cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -25,29 +25,34 @@ function Cart() {
       </h3>
 
       <div className="row bag-wrapper">
-        <div className="col-12 mt-5">
-          <div className="card py-3 px-3">
-            <div className="col-12 text-center">
-              <img
-                src="./images/1.jpg"
-                alt="Product Image"
-                className="cart-img-top w-75"
-              />
-            </div>
-            <div className="card-body d-flex flex-column justify-content-center align-items-center">
-              <p className="card-text">Samsung A51</p>
-              <p className="price">12000</p>
-              <br />
-              <a href="#" className="btn btn-danger">
-                Buy
-              </a>
-              <a href="#" className="btn btn-outline-dark mt-3 text-capitalize">
-                More Info
-              </a>
-              <p className="number">12</p>
+        {contextData.userCart.map((product) => (
+          <div className="col-12 mt-5" key={product.id}>
+            <div className="card py-3 px-3">
+              <div className="col-12 text-center">
+                <img
+                  src="./images/1.jpg"
+                  alt="Product Image"
+                  className="cart-img-top w-75"
+                />
+              </div>
+              <div className="card-body d-flex flex-column justify-content-center align-items-center">
+                <p className="card-text">{product.title}</p>
+                <p className="price">{product.price}</p>
+                <br />
+                <a href="#" className="btn btn-danger">
+                  Buy
+                </a>
+                <a
+                  href="#"
+                  className="btn btn-outline-dark mt-3 text-capitalize"
+                >
+                  More Info
+                </a>
+                <p className="number">{product.count}</p>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </aside>
   );
